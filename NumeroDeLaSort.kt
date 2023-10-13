56ad7884ba3b4d6499d623edf0e34be1f69e71b8
Toumai <daniel.salazar.7e7@itb.cat>
2023-10-13 23:15:55 +0200

    final de todo

--- NumeroDeLaSort.kt
+++ NumeroDeLaSort.kt
@@ -1,4 +1,62 @@
 import java.util.*
-fun main () {
 
+fun main() {
+    val scan = Scanner(System.`in`)
+    var dataValida = false
+    var data:Int
+    var any:Int
+    var mes:Int
+    var dia:Int
+    do {
+        print("Introdueixi una data de neixament en format ddmmaaaa valida: ")
+        do {
+            if (!scan.hasNextInt()) {
+                print("Introdueixi un enter en format ddmmaaaa: ")
+                scan.next()
+            }
+        } while (!scan.hasNextInt())
+        data = scan.nextInt()
+        any = data % 10000
+        mes = ((data % 1000000) - (any)) / 10000
+        dia = ((data % 100000000) - (mes)) / 1000000
+
+        if (any in 1..2023 || mes in 1..12) {
+            when (mes) {
+                1, 3, 5, 7, 8, 10, 12 -> {
+                    if (dia in 1..31) {
+                        dataValida = true
+                    }
+                }
+
+                4, 6, 9, 11 -> {
+                    if (dia in 1..30) {
+                        dataValida = true
+                    }
+                }
+
+                2 -> {
+                    if ((any % 4 != 0) || ((any % 100 != 0) && (any % 4 == 0))) {
+                        if (dia in 1..28) {
+                            dataValida = true
+                        }
+                    } else {
+                        if (dia in 1..29) {
+                            dataValida = true
+                        }
+                    }
+                }
+            }
+        }
+    } while (!dataValida)
+
+    var numeroSort = data
+    while (numeroSort > 9) {
+        var sumaDigits:Int = 0
+        while (numeroSort > 0) {
+            sumaDigits += numeroSort % 10
+            numeroSort /= 10
+        }
+        numeroSort = sumaDigits
+    }
+    println("El teu numero de la sort es $numeroSort")
 }
