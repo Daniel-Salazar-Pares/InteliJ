f5dd53edecfecec790be75420a040616854f9d6a
Toumai <daniel.salazar.7e7@itb.cat>
2023-10-13 17:21:02 +0200

    primers 3 problemes acabats

---
+++ Identificador de primers.kt
@@ -0,0 +1,44 @@
+import java.util.*
+fun main () {
+    val scan = Scanner(System.`in`)
+    var numero = 0
+    var divisors = 0
+    var enter = false
+    println("Insereixi un numero enter major a 1")
+    do {
+        if (!scan.hasNextInt()) {
+            println("Ingresi un enter")
+            scan.next()
+        } else {
+            numero = scan.nextInt()
+            if (numero <= 1) {
+                println("El numero ha de ser major a 1")
+            }
+        }
+    } while (numero <= 1)
+    for (valor in 1..numero) {
+        if (numero%valor == 0){
+            ++divisors
+        }
+    }
+    when {
+        divisors == 2 -> println("El numero es primer")
+        divisors > 2 -> {
+            var seguentPrimer = numero
+            var divisorsSeguentPrimer = 0
+            do {
+                seguentPrimer++
+                divisorsSeguentPrimer = 0
+                for (valor in 1..seguentPrimer) {
+                    if (seguentPrimer % valor == 0) {
+                        ++divisorsSeguentPrimer
+                    }
+                }
+                if (divisorsSeguentPrimer == 2) {
+                    enter = true
+                }
+            } while (!enter)
+            println("El numero no es un enter, pero el seguent primer es $seguentPrimer")
+        }
+    }
+}
